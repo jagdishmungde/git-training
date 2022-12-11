14f7ce86c400f9fa6592e0c0289fa08852034614
jagdishmungde <jagdish.mungde06@gmail.com>
2022-12-11 12:56:13 +0530

    3rd: commit list sort+uppercase+only show LAND

--- src/com/training/firstgit.java
+++ src/com/training/firstgit.java
@@ -1,8 +1,14 @@
 package com.training;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class firstgit {
 	public static void main(String[] args) {
+		List <String>asList=Arrays.asList("India","pakistan","iceland","finland","swizerland");
 		
+		asList.stream().sorted().map(a -> a.toUpperCase()).filter(a -> a.contains("LAND")).forEach(System.out::println);
+//		asList.stream().sorted().map(abc-> abc.toUpperCase()).forEach(System.out::println);
 	}
 
 }
