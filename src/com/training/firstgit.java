67d3dde48b8880698e78ff559688123639194dd7
jagdishmungde <jagdish.mungde06@gmail.com>
2022-12-12 14:49:41 +0530

    update last

--- src/com/training/firstgit.java
+++ src/com/training/firstgit.java
@@ -9,9 +9,19 @@ public class firstgit {
 		List <String>asList=Arrays.asList("india","pakistan","iceland","finland","swizerland");
 		
 		asList.stream().sorted().map(a -> a.toUpperCase()).filter(a -> a.contains("LAND")).forEach(System.out::println);
-//		asList.stream().sorted().map(abc-> abc.toUpperCase()).forEach(System.out::println);
+		asList.stream().sorted().map(abc-> abc.toUpperCase()).forEach(System.out::println);
 		System.out.println("\n khkljfiogjuh ");
 		asList.parallelStream().sorted().filter(a -> a.startsWith("i")).map(a -> a.toUpperCase()).forEach(System.out::println);
+		
+		int a=1;
+		int b=0;
+		int c;
+//		c=a/b;
+//		System.out.println(c);
+		
+		List <Integer> asInt=Arrays.asList(10,45,36,8,7,72,65);
+		
+		asInt.stream().filter(num -> num%2==0).forEach(System.out::println);
 		
 	}
 
